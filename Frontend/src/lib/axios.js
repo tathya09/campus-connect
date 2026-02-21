6db4414c2eac304b8cf8ae7984898a17fe6f5c13
tathya09 <106487768+tathya09@users.noreply.github.com>
2026-02-21 23:52:51 +0530

    chnaged url

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  //baseURL: "http://localhost:3000",
+  baseURL: "https://campus-connect-bn54.vercel.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
